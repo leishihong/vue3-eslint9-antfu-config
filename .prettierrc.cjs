fe1c12a331e7b3f55fcda9d31127b68397fd6ac0
beichen <leishihong@jd.com>
2025-01-04 22:37:15 +0800

    fix: 测试eslint 规则

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+	$schema: 'https://json.schemastore.org/prettierrc',
+	semi: false,
+	singleQuote: true,
+	printWidth: 100,
+	tabWidth: 2,
+	useTabs: true,
+	trailingComma: 'none',
+	jsxSingleQuote: true
+}
