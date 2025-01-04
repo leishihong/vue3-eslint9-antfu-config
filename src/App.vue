4dfa5ca1c8be677f55bd0bcfb4e2c9410e74860a
beichen <leishihong@jd.com>
2025-01-04 23:28:27 +0800

    fix: 测试eslint 规则

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ console.log(`output->count`, count)
 		<img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 		<div class="wrapper">
 			<HelloWorld msg="You did it!" />
-			<Test />
+			<test />
 			<nav>
 				<RouterLink to="/">Home</RouterLink>
 				<RouterLink to="/about">About</RouterLink>
